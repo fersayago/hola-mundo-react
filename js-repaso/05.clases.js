1464239b0fa2db33e8e021fac517860aca098688
fersayago <sayago.fernando@gmail.com>
2021-11-02 23:31:07 -0300

    comenzado clases

---
+++ js-repaso/05.clases.js
@@ -0,0 +1,18 @@
+// expresion de clase
+// definimos una clase y se la asignamos a una constante
+const Rectangulo = class R {
+
+}
+
+// declaracion de clase
+class Humano {
+
+}
+
+function Cuadrado() {
+  console.log(Cuadrado, Rectangulo);
+}
+
+Cuadrado();
+
+const r = new Rectangulo()
